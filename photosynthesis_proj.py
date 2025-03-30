ed7373c5f8d0fd2699511357bbaf1cfe38a4f7ec
maryam-m010 <mmaryam7@outlook.com>
2025-03-30 17:23:50 +0100

    Update photosynthesis_proj.py

--- photosynthesis_proj.py
+++ photosynthesis_proj.py
@@ -2,7 +2,7 @@ import pygame, sys
 
 pygame.init()
 
-screen = pygame.display.set_mode((600, 600))
+screen = pygame.display.set_mode((800, 300))
 pygame.display.set_caption("photosynthesis")
 screen.fill("white")
 
@@ -33,16 +33,51 @@ class Photosynthesis:
                 self.c_radius = radius
                 self.c_width = width
                 self.c_surface = screen
-            def draw(self):
+            def draw(self, screen):
                 pygame.draw.circle(self.c_surface, self.c_color, self.c_pos, self.c_radius, self.c_width)
+                screen.draw.text("Carbon Dioxide                +", (self.c_pos[0], self.c_pos[1] + 20) )
             
         #oxygen1 = Circle("blue", (350,250), 15, 0)
         #oxygen2 = Circle("blue", (350,250), 15, 0)
+
+    class Glucose:
+        def __init__():
+            pass
             
-carbon = Photosynthesis("sunlight", "H2O", "yellow", (300,300), 20, 0)
+carbon = Photosynthesis("sunlight", "H2O", "yellow", (100,150), 20, 0)
+oxygen1 = Photosynthesis("sunlight", "H2O", "blue", (85,165), 10, 0)
+oxygen2 = Photosynthesis("sunlight", "H2O", "blue", (115, 165), 10, 0)
+
+oxygen3 = Photosynthesis("sunlight", "H2O", "blue", (165, 150), 20, 0)
+hydrogen1 = Photosynthesis("sunlight", "H2O", "grey", (150, 135), 10, 0)
+hydrogen2 = Photosynthesis("sunlight", "H2O", "grey", (180, 135), 10, 0)
+
+oxygen4 = Photosynthesis("sunlight", "H2O", "blue", (360, 150), 20, 0)
+oxygen5 = Photosynthesis("sunlight", "H2O", "blue", (375, 130), 20, 0)
 
 carbon_circle1 = carbon.carbon_dioxide.circle
-carbon_circle1.draw()
+carbon_circle1.draw(screen)
+
+oxygen_circle1 = oxygen1.carbon_dioxide.circle
+oxygen_circle1.draw(screen)
+
+oxygen_circle2 = oxygen2.carbon_dioxide.circle
+oxygen_circle2.draw(screen)
+
+oxygen_circle3 = oxygen3.carbon_dioxide.circle
+oxygen_circle3.draw(screen)
+
+hydrogen_circle1 = hydrogen1.carbon_dioxide.circle
+hydrogen_circle1.draw(screen)
+
+hydrogen_circle2 = hydrogen2.carbon_dioxide.circle
+hydrogen_circle2.draw(screen)
+
+oxygen_circle4 = oxygen4.carbon_dioxide.circle
+oxygen_circle4.draw(screen)
+
+oxygen_circle5 = oxygen5.carbon_dioxide.circle
+oxygen_circle5.draw(screen)
 
 pygame.display.update()
 
